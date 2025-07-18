9d4cc58109425c450d594b950d9f85018350a361
Jayshankar yadav <71151120+jay-cmk@users.noreply.github.com>
2025-07-18 13:55:25 +0530

    change url

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ import { toast } from 'react-toastify';
 export const DoctorContext=createContext()
 
 const DoctorContextProvider=(props)=>{
-    const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://carepoint-u5lv.onrender.com";
 
 
     const [dToken,setDToken] = useState(localStorage.getItem("dToken") || "");
